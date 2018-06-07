e3ae650d7740ce50e822713121a1f9d1be0b0795
romson <romerolijkwan@gmail.com>
2018-06-07 18:06:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Change testVariable to redefine your test variable */
+var testVariable = 'ABC';
+var isUndefined = (testVariable === undefined);
+var isNull = (testVariable === null);
+var isEmpty = (testVariable === '');
+
+/* Display test results on your HTML page */
+document.getElementById('isUndefined').innerHTML = isUndefined;
+document.getElementById('isNull').innerHTML = isNull;
+document.getElementById('isEmpty').innerHTML = isEmpty;
+
