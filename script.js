dd4e6d35b6c5d452b103466716bca11d8dba614f
Romero Lijkwan <romerolijkwan@gmail.com>
2018-06-07 20:27:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-/* Change testVariable to redefine your test variable */
+// Change testVariable to redefine your test variable
 var testVariable = 'ABC';
 var isUndefined = (testVariable === undefined);
 var isNull = (testVariable === null);
 var isEmpty = (testVariable === '');
 
-/* Display test results on your HTML page */
+// Display test results on your HTML page
 document.getElementById('isUndefined').innerHTML = isUndefined;
 document.getElementById('isNull').innerHTML = isNull;
 document.getElementById('isEmpty').innerHTML = isEmpty;
